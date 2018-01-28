dfa2433c9d42b82dc72d1679b68e80aaa093a8a5
Nguyen Phuc Thien An <npta97@gmail.com>
2018-01-28 11:00:26 +0700

    Add Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const config = require('./config');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoURI);
 
 app.listen(config.port, () => {
   console.log(`Server listening on PORT ${config.port}`);
